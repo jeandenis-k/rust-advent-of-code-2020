cab3ff4af870a9100f4dacc42021004cd12c3980
Jean-Denis Koeck <jdkoeck@gmail.com>
2020-12-20 23:39:24 +0100

    Implement part 2 of day 2

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -15,19 +15,27 @@ struct PasswordLine {
 
 fn main() {
     let file = std::fs::File::open("input").unwrap();
-    let lines = std::io::BufReader::new(file).lines().map(|line| {
-        let line = line.unwrap();
-        let mut parts = line.split(":");
-        let policy: Policy = parse_policy_string(parts.next().unwrap().to_string());
+    let lines = std::io::BufReader::new(file)
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let mut parts = line.split(":");
+            let policy: Policy = parse_policy_string(parts.next().unwrap().to_string());
 
-        let password: String = parts.next().unwrap().to_string().split_off(1);
+            let password: String = parts.next().unwrap().to_string().split_off(1);
 
-        return PasswordLine { policy, password };
-    });
+            return PasswordLine { policy, password };
+        })
+        .collect::<Vec<_>>();
 
     println!(
         "{} passwords are valid!",
-        lines.filter(|line| is_valid(line)).count()
+        lines.iter().filter(|line| is_valid(line)).count()
+    );
+
+    println!(
+        "{} passwords are valid with interpretation 2!",
+        lines.iter().filter(|line| is_valid_2(line)).count()
     );
 }
 
@@ -47,4 +55,14 @@ fn parse_policy_string(policy: String) -> Policy {
 fn is_valid(PasswordLine { policy, password }: &PasswordLine) -> bool {
     let count = password.chars().filter(|&c| c == policy.letter).count();
     policy.min <= count && count <= policy.max
+}
+
+fn is_valid_2(PasswordLine { policy, password }: &PasswordLine) -> bool {
+    let chars = password.chars().collect::<Vec<_>>();
+    return [policy.min, policy.max]
+        .iter()
+        .map(|index| chars[index - 1] == policy.letter)
+        .filter(|&b| b)
+        .count()
+        == 1;
 }
