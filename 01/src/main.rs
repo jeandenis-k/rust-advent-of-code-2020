3e54e56fe00ae05c38db28814a859b4a292de555
Jean-Denis Koeck <jdkoeck@gmail.com>
2020-12-20 22:47:34 +0100

    Solve part 2 of day one

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -9,13 +9,28 @@ fn main() {
         .map(|line| line.unwrap().parse::<i64>().unwrap())
         .collect();
 
-    println!("Solution is {}", solve_puzzle(numbers).unwrap());
+    println!("Solution of part 1 is {}", solve_puzzle(&numbers).unwrap());
+    println!("Solution of part 2 is {}", solve_part2(&numbers).unwrap());
 }
 
-fn solve_puzzle(numbers: Vec<i64>) -> Option<i64> {
+fn solve_puzzle(numbers: &Vec<i64>) -> Option<i64> {
     return numbers.iter().find_map(|i1| {
         numbers
             .iter()
             .find_map(|i2| if i1 + i2 == 2020 { Some(i1 * i2) } else { None })
+    });
+}
+
+fn solve_part2(numbers: &Vec<i64>) -> Option<i64> {
+    return numbers.iter().find_map(|i1| {
+        numbers.iter().find_map(|i2| {
+            numbers.iter().find_map(|i3| {
+                if i1 + i2 + i3 == 2020 {
+                    Some(i1 * i2 * i3)
+                } else {
+                    None
+                }
+            })
+        })
     });
 }
