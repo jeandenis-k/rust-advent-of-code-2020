be953dcddccc50e83875da529f1f5014826cd6c6
Jean-Denis Koeck <jdkoeck@gmail.com>
2020-12-21 22:04:19 +0100

    Solve part 2 of day 3

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -2,7 +2,7 @@ use std::io::BufRead;
 use std::vec::Vec;
 
 fn main() {
-    let file = std::fs::File::open("example_map").unwrap();
+    let file = std::fs::File::open("input").unwrap();
     let map: Map = Map(std::io::BufReader::new(file)
         .lines()
         .map(|line| {
@@ -22,6 +22,20 @@ fn main() {
 
     let solution = map.count_trees(&Slope { right: 3, down: 1 });
     println!("Encountered {} trees", solution);
+
+    let slopes = [
+        Slope { right: 1, down: 1 },
+        Slope { right: 3, down: 1 },
+        Slope { right: 5, down: 1 },
+        Slope { right: 7, down: 1 },
+        Slope { right: 1, down: 2 },
+    ];
+
+    let solution2 = slopes
+        .iter()
+        .map(|slope| map.count_trees(&slope))
+        .fold(1, |acc, count| acc * count);
+    println!("Slope counts multiplied equals {:?}", solution2);
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -42,9 +56,19 @@ impl Map {
         return self
             .0
             .iter()
+            .step_by(slope.down)
             .enumerate()
             // .for_each(|(index, line)| println!("{}", index * 3 % line.len()))
-            .map(|(index, line)| line[index * slope.right % line.len()])
+            .map(|(index, line)| {
+                let location = line[index * slope.right % line.len()];
+                println!(
+                    "{}, {}: {:?}",
+                    index,
+                    index * slope.right % line.len(),
+                    location
+                );
+                return location;
+            })
             .filter(|&location| location == MapLocation::Tree)
             .count();
     }
