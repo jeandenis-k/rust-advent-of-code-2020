2bea1b1339c54ad6ebf01edbda2c46ebe357226d
Jean-Denis Koeck <jdkoeck@gmail.com>
2020-12-22 22:22:45 +0100

    Solve day 7 part 1

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -1,47 +1,66 @@
 use regex::Regex;
+use std::collections::HashMap;
 use std::io::BufRead;
 
 #[derive(Debug)]
-struct Rule {
-    color: String,
-    contained: Vec<(usize, String)>,
-}
+struct BagMap(HashMap<String, Vec<(usize, String)>>);
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     let file = std::fs::File::open(&args[1]).unwrap();
-    let rules: Vec<Rule> = std::io::BufReader::new(file)
-        .lines()
-        .filter_map(Result::ok)
-        .map(|line| {
-            let re = Regex::new(r"(.*) bags contain (.*)").unwrap();
-            let matches = re.captures(&line).unwrap();
-            let color = matches.get(1).unwrap().as_str().to_string();
+    let bag_map: BagMap = BagMap(
+        std::io::BufReader::new(file)
+            .lines()
+            .filter_map(Result::ok)
+            .map(|line| {
+                let re = Regex::new(r"(.*) bags contain (.*)").unwrap();
+                let matches = re.captures(&line).unwrap();
+                let color = matches.get(1).unwrap().as_str().to_string();
 
-            let contained_string = matches.get(2).unwrap().as_str();
-            if contained_string == "no other bags." {
-                Rule {
-                    color,
-                    contained: vec![],
+                let contained_string = matches.get(2).unwrap().as_str();
+                if contained_string == "no other bags." {
+                    (color, vec![])
+                } else {
+                    let contained_parts: Vec<_> = contained_string
+                        .split(",")
+                        .map(|part| {
+                            let re = Regex::new(r"(\d) (.*) bag").unwrap();
+                            let matches = re.captures(&part).unwrap();
+                            (
+                                matches.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+                                matches.get(2).unwrap().as_str().to_string(),
+                            )
+                        })
+                        .collect();
+                    (color, contained_parts)
                 }
-            } else {
-                let contained_parts: Vec<_> = contained_string
-                    .split(",")
-                    .map(|part| {
-                        let re = Regex::new(r"(\d) (.*) bag").unwrap();
-                        let matches = re.captures(&part).unwrap();
-                        (
-                            matches.get(1).unwrap().as_str().parse::<usize>().unwrap(),
-                            matches.get(2).unwrap().as_str().to_string(),
-                        )
-                    })
-                    .collect();
-                Rule {
-                    color,
-                    contained: contained_parts,
-                }
-            }
-        })
-        .collect();
-    println!("{:?}", rules);
+            })
+            .collect(),
+    );
+    println!("{:?}", bag_map);
+
+    let mut shiny_map: HashMap<String, bool> = HashMap::new();
+    println!(
+        "Solution is {}",
+        bag_map
+            .0
+            .keys()
+            .filter(|color| bag_map.color_contains_shiny(&mut shiny_map, color))
+            .count()
+    );
+}
+
+impl BagMap {
+    fn color_contains_shiny(
+        self: &BagMap,
+        shiny_map: &mut HashMap<String, bool>,
+        color: &String,
+    ) -> bool {
+        let children = self.0.get(color).unwrap();
+        let has_direct_shiny_gold = children.iter().any(|(_, color)| color == "shiny gold");
+        has_direct_shiny_gold
+            || children
+                .iter()
+                .any(|(_, color)| self.color_contains_shiny(shiny_map, color))
+    }
 }
