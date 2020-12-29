a869c2136c5976052b06d4997a6c785f160a9376
Jean-Denis Koeck <jdkoeck@gmail.com>
2020-12-29 22:09:33 +0100

    Solve part 2 of day 9

--- 09/src/main.rs
+++ 09/src/main.rs
@@ -22,7 +22,14 @@ fn main() {
     let part1_solution = xmas_it
         .find_map(|(value, valid)| if !valid { Some(value) } else { None })
         .unwrap();
-    println!("Solution of part 1 is {}", part1_solution)
+    println!("Solution of part 1 is {}", part1_solution);
+
+    let contiguous_nums =
+        find_contiguous_numbers_adding_up_to_step1(&lines, part1_solution).unwrap();
+    println!(
+        "Solution of part 2 is {}",
+        contiguous_nums.iter().min().unwrap() + contiguous_nums.iter().max().unwrap()
+    )
 }
 
 fn generate_valid_sums(preamble: &[i64]) -> HashSet<i64> {
@@ -31,6 +38,27 @@ fn generate_valid_sums(preamble: &[i64]) -> HashSet<i64> {
         .enumerate()
         .flat_map(|(index, &n1)| preamble[index + 1..].iter().map(move |n2| n1 + n2))
         .collect()
+}
+
+fn find_contiguous_numbers_adding_up_to_step1(numbers: &Vec<i64>, step1: i64) -> Option<Vec<i64>> {
+    numbers.iter().enumerate().find_map(|(index, n)| {
+        println!("Testing starting from  {}", n);
+        let mut sum = *n;
+        let contiguous_chain: Vec<_> = numbers[index + 1..]
+            .iter()
+            .cloned()
+            .take_while(|n2| {
+                sum = sum + *n2;
+                println!("Sum is now {}", sum);
+                sum < step1
+            })
+            .collect();
+        if sum == step1 {
+            Some(numbers[index..=index + contiguous_chain.len() + 1].to_vec())
+        } else {
+            None
+        }
+    })
 }
 
 impl<'a> Iterator for XmasIterator<'a> {
@@ -81,17 +109,30 @@ mod tests {
     }
 
     #[test]
+    fn test_part2() {
+        let values = parse_input_example();
+        assert_eq!(
+            find_contiguous_numbers_adding_up_to_step1(&values, 127).unwrap(),
+            vec![15, 25, 47, 40]
+        );
+    }
+
+    #[test]
     fn find_invalid_number_in_example() {
-        let file = File::open("./input_example").unwrap();
-        let values: Vec<_> = BufReader::new(file)
-            .lines()
-            .filter_map(Result::ok)
-            .map(|line| line.parse::<i64>().unwrap())
-            .collect();
+        let values = parse_input_example();
         let mut xmas_it = XmasIterator::new(&values, 5);
         assert_eq!(
             xmas_it.find_map(|(value, valid)| if !valid { Some(value) } else { None }),
             Some(127)
         );
+    }
+
+    fn parse_input_example() -> Vec<i64> {
+        let file = File::open("./input_example").unwrap();
+        BufReader::new(file)
+            .lines()
+            .filter_map(Result::ok)
+            .map(|line| line.parse::<i64>().unwrap())
+            .collect()
     }
 }
