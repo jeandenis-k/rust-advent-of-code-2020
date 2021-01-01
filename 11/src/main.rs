fa3bb1850ac127b8b9cdc854b7ea1ed53b2e2598
Jean-Denis Koeck <jdkoeck@gmail.com>
2021-01-01 18:35:15 +0100

    Yield area on next()

--- 11/src/main.rs
+++ 11/src/main.rs
@@ -1,10 +1,9 @@
 use std::io::BufRead;
 use std::io::{self};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct WaitingArea {
     cells: Vec<String>,
-    width: usize,
 }
 
 fn main() {
@@ -17,29 +16,26 @@ fn main() {
 impl WaitingArea {
     fn new(it: impl Iterator<Item = String>) -> WaitingArea {
         let cells: Vec<_> = it.collect();
-        let width = cells[0].len();
-        WaitingArea { cells, width }
+        WaitingArea { cells }
     }
 }
 
 impl Iterator for WaitingArea {
-    type Item = (Vec<String>, Vec<String>);
+    type Item = (WaitingArea, WaitingArea);
 
     fn next(self: &mut WaitingArea) -> Option<Self::Item> {
-        let old = self.cells.clone();
+        let old = self.clone();
         for (i, line) in self.cells.iter_mut().enumerate() {
             unsafe {
                 for (j, cell) in line.as_bytes_mut().iter_mut().enumerate() {
                     if *cell == b'L' {
                         let no_adjacent_occupied_seats: bool =
-                            adjacent_cells(&old, self.width, i, j)
-                                .iter()
-                                .all(|c| *c != b'#');
+                            adjacent_cells(&old.cells, i, j).iter().all(|c| *c != b'#');
                         if no_adjacent_occupied_seats {
                             *cell = b'#';
                         }
                     } else if *cell == b'#' {
-                        let adjacent_occupied_count = adjacent_cells(&old, self.width, i, j)
+                        let adjacent_occupied_count = adjacent_cells(&old.cells, i, j)
                             .iter()
                             .filter(|c| **c == b'#')
                             .count();
@@ -50,13 +46,14 @@ impl Iterator for WaitingArea {
                 }
             }
         }
-        Some((old, self.cells.clone()))
+        Some((old, self.clone()))
     }
 }
 
-fn adjacent_cells<'a>(cells: &'a Vec<String>, width: usize, i: usize, j: usize) -> Vec<u8> {
+fn adjacent_cells<'a>(cells: &'a Vec<String>, i: usize, j: usize) -> Vec<u8> {
     let i = i as isize;
     let j = j as isize;
+    let width = cells[0].len();
     let lines: Vec<isize> = vec![i - 1, i, i + 1];
     let cols: Vec<isize> = vec![j - 1, j, j + 1];
     lines
@@ -69,10 +66,7 @@ fn adjacent_cells<'a>(cells: &'a Vec<String>, width: usize, i: usize, j: usize) 
                 && *l >= 0
                 && *l < cells.len() as isize
         })
-        .map(|(i, j)| {
-            dbg!(i, j);
-            cells[i as usize].as_bytes()[j as usize]
-        })
+        .map(|(i, j)| cells[i as usize].as_bytes()[j as usize])
         .collect()
 }
 
@@ -84,6 +78,6 @@ mod tests {
     fn test_adjacent_cells() {
         let area_string = include_str!("../input_example");
         let area = WaitingArea::new(area_string.lines().map(|line| line.to_string()));
-        assert_eq!(adjacent_cells(&area.cells, 10, 0, 9), [b'L', b'L', b'L'])
+        assert_eq!(adjacent_cells(&area.cells, 0, 9), [b'L', b'L', b'L'])
     }
 }
