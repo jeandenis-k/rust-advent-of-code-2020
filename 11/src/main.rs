d028e97742a8e00e53e6df0b914c060d4d554ece
Jean-Denis Koeck <jdkoeck@gmail.com>
2021-01-01 17:54:52 +0100

    Begin implementing day 11

--- 11/src/main.rs
+++ 11/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self};
 
 #[derive(Debug)]
 struct WaitingArea {
-    cells: Vec<Vec<char>>,
+    cells: Vec<String>,
     width: usize,
 }
 
@@ -18,22 +18,53 @@ fn main() {
 
 impl WaitingArea {
     fn new(it: impl Iterator<Item = String>) -> WaitingArea {
-        let cells: Vec<Vec<char>> = it.map(|s| s.chars().collect()).collect();
+        let cells: Vec<_> = it.collect();
         let width = cells[0].len();
         WaitingArea { cells, width }
     }
 }
 
 impl Iterator for WaitingArea {
-    type Item = (Vec<Vec<char>>, Vec<Vec<char>>);
+    type Item = (Vec<String>, Vec<String>);
 
     fn next(self: &mut WaitingArea) -> Option<Self::Item> {
         let old = self.cells.clone();
         for (i, line) in self.cells.iter_mut().enumerate() {
-            for (j, cell) in line.iter_mut().enumerate() {
-                *cell = 'X';
+            unsafe {
+                for (j, cell) in line.as_bytes_mut().iter_mut().enumerate() {
+                    if *cell == b'L' {
+                        let no_occupied_seats: bool =
+                            adjacent_cells(&old, i, j).iter().all(|c| *c != b'#');
+                        if no_occupied_seats {
+                            *cell = b'#';
+                        }
+                    }
+                }
             }
         }
         Some((old, self.cells.clone()))
     }
+}
+
+fn adjacent_cells<'a>(cells: &'a Vec<String>, i: usize, j: usize) -> Vec<u8> {
+    let lines = if i == 0 {
+        vec![0, 1]
+    } else {
+        vec![i - 1, i, i + 1]
+    };
+    let cols = if j == 0 {
+        vec![0, 1]
+    } else {
+        vec![j - 1, j, j + 1]
+    };
+    lines
+        .iter()
+        .flat_map(|line| cols.iter().map(move |col| (*line, *col)))
+        .filter(|(i, j)| i != j)
+        .filter_map(|(i, j)| {
+            cells
+                .get(i)
+                .and_then(|line| line.as_bytes().get(j).map(|c| *c))
+        })
+        .collect()
 }
