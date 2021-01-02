d2dcc08079619635caec17959944adc6d95a02de
Jean-Denis Koeck <jdkoeck@gmail.com>
2021-01-02 15:08:11 +0100

    Start parsing day 12

---
+++ 12/src/main.rs
@@ -0,0 +1,115 @@
+use std::str::FromStr;
+use Direction::*;
+use NavAction::*;
+
+struct Ship {
+    east_pos: i32,
+    north_pos: i32,
+    dir_faced: Direction,
+}
+
+enum Direction {
+    East,
+    South,
+    West,
+    North,
+}
+
+#[derive(Debug, PartialEq)]
+struct NavInstruction {
+    action: NavAction,
+    value: i32,
+}
+
+#[derive(Debug, PartialEq)]
+enum NavAction {
+    N,
+    S,
+    E,
+    W,
+    L,
+    R,
+    F,
+}
+
+impl FromStr for NavAction {
+    type Err = ();
+    fn from_str(s: &str) -> Result<NavAction, ()> {
+        match s {
+            "N" => Ok(N),
+            "S" => Ok(S),
+            "E" => Ok(E),
+            "W" => Ok(W),
+            "L" => Ok(L),
+            "R" => Ok(R),
+            "F" => Ok(F),
+            _ => Err(()),
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+impl Ship {
+    fn new() -> Ship {
+        Ship {
+            east_pos: 0,
+            north_pos: 0,
+            dir_faced: East,
+        }
+    }
+}
+
+impl NavInstruction {
+    fn parse(it: impl Iterator<Item = String>) -> impl Iterator<Item = NavInstruction> {
+        it.map(|line| {
+            let action: NavAction = line[0..=0].parse().unwrap();
+            let value = line[1..].parse().unwrap();
+            NavInstruction { action, value }
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static EXAMPLE: &str = "\
+F10
+N3
+F7
+R90
+F11
+";
+
+    #[test]
+    fn test_parse_instructions() {
+        assert_eq!(
+            NavInstruction::parse(EXAMPLE.lines().map(|str| str.to_string())).collect::<Vec<_>>(),
+            vec![
+                NavInstruction {
+                    action: F,
+                    value: 10
+                },
+                NavInstruction {
+                    action: N,
+                    value: 3
+                },
+                NavInstruction {
+                    action: F,
+                    value: 7
+                },
+                NavInstruction {
+                    action: R,
+                    value: 90
+                },
+                NavInstruction {
+                    action: F,
+                    value: 11
+                },
+            ]
+        )
+    }
+}
