5b3afaa79dd04f297a0b810982798eab4e44770d
Jean-Denis Koeck <jdkoeck@gmail.com>
2021-01-09 12:16:04 +0100

    First stab at part 2

--- 13/src/main.rs
+++ 13/src/main.rs
@@ -1,4 +1,6 @@
+use std::convert::TryFrom;
 use std::str::FromStr;
+use BusEntry::*;
 
 #[derive(Debug, PartialEq)]
 struct Notes {
@@ -6,13 +8,14 @@ struct Notes {
     bus_ids: Vec<BusEntry>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum BusEntry {
     Id(i32),
     X,
 }
 fn main() {
     println!("{:?}", parse(INPUT).unwrap().solve_part1());
+    println!("{:?}", parse(INPUT).unwrap().solve_part2());
 }
 
 static INPUT: &str = include_str!("../input");
@@ -23,7 +26,6 @@ fn parse(input: &str) -> Option<Notes> {
     let bus_ids = lines
         .next()?
         .split(",")
-        .filter(|s| *s != "x")
         .map(|s| s.parse())
         .filter_map(Result::ok)
         .collect();
@@ -49,6 +51,39 @@ impl Notes {
             })
         })
     }
+    fn solve_part2(self: &Notes) -> Option<i64> {
+        let first = match self.bus_ids[0] {
+            Id(id) => id,
+            X => 1,
+        };
+        (100000000000000_i64..).find_map(|n| {
+            dbg!(n);
+            if self
+                .bus_ids
+                .iter()
+                .enumerate()
+                .all(|(i, entry)| match entry {
+                    BusEntry::Id(bus_id) => (n + i64::try_from(i).unwrap()) % (*bus_id as i64) == 0,
+                    BusEntry::X => true,
+                })
+            {
+                Some(n)
+            } else {
+                None
+            }
+        })
+    }
+
+    fn max(self: &Notes) -> i32 {
+        self.bus_ids
+            .iter()
+            .map(|entry| match entry {
+                X => 0,
+                Id(id) => *id,
+            })
+            .max()
+            .unwrap()
+    }
 }
 
 impl FromStr for BusEntry {
@@ -72,10 +107,7 @@ mod tests {
             parse(INPUT_EXAMPLE),
             Some(Notes {
                 earliest: 939,
-                bus_ids: vec![7, 13, 59, 31, 19]
-                    .iter()
-                    .map(|id| BusEntry::Id(*id))
-                    .collect()
+                bus_ids: vec![Id(7), Id(13), X, X, Id(59), X, Id(31), Id(19)]
             })
         )
     }
@@ -83,5 +115,10 @@ mod tests {
     #[test]
     fn test_solve_part1() {
         assert_eq!(parse(INPUT_EXAMPLE).unwrap().solve_part1(), Some(295))
+    }
+
+    #[test]
+    fn test_solve_part2() {
+        assert_eq!(parse("12\n17,x,13,19").unwrap().solve_part2(), Some(3417))
     }
 }
